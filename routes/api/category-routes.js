1cb87adbf95266f86e391ae48938ad945734c7ef
Max McDonough <max.mcdonough@gmail.com>
2022-08-02 17:41:01 -0400

    Added category routes: get, post, and put

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,24 +1,68 @@
 const router = require('express').Router();
+const { restart } = require('nodemon');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Find all Categories
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  } 
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Find one Category by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!categoryData) {
+      res.status(400).json({message: 'There is no category with that ID!'});
+      return;
+    } 
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Create a new Category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Update a Category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const checkCategoryId = await Category.findByPk(req.params.id);
+    if (!checkCategoryId) {
+      res.status(400).json({message: 'There is no category with that ID!'});
+      return;
+    };
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    const updatedCategoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(updatedCategoryData);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
